a1316f492b5700eed98310019d0af7a03dde062b
ditas <ditasandditas@gmail.com>
2018-11-06 17:17:39 +0300

    types added

--- src/types_helper.erl
+++ src/types_helper.erl
@@ -11,7 +11,9 @@
 
 %% API
 -export([
-    val_to_list/1
+    val_to_list/1,
+    val_to_float/1,
+    val_to_integer/1
 ]).
 
 val_to_list(Val) when is_float(Val) ->
@@ -22,3 +24,46 @@ val_to_list(Val) when is_binary(Val) ->
     binary_to_list(Val);
 val_to_list(Val) ->
     Val.
+
+val_to_float(Val) when is_float(Val) ->
+    Val;
+val_to_float(Val) when is_integer(Val) ->
+    float(Val);
+val_to_float(Val) when is_binary(Val) ->
+    case (catch binary_to_float(Val)) of
+        Float ->
+            Float;
+        {'EXIT', _Reason} ->
+            case is_numeric(Val) of
+                true ->
+                    float(binary_to_integer(Val));
+                false ->
+                    0.0
+            end
+    end;
+val_to_float(Val) when is_list(Val) ->
+    val_to_float(list_to_binary(Val)).
+
+val_to_integer(Val) when is_integer(Val) ->
+    Val;
+val_to_integer(Val) when is_float(Val) ->
+    round(Val);
+val_to_integer(Val) when is_binary(Val) ->
+    case (catch binary_to_integer(Val)) of
+        Int ->
+            Int;
+        {'EXIT', _Reason} ->
+            case is_numeric(Val) of
+                true ->
+                    round(binary_to_float(Val));
+                false ->
+                    0
+            end
+    end;
+val_to_integer(Val) when is_list(Val) ->
+    val_to_integer(list_to_binary(Val)).
+
+is_numeric(Val) ->
+    Float = (catch binary_to_float(Val)),
+    Int = (catch binary_to_integer(Val)),
+    is_number(Float) orelse is_number(Int).
