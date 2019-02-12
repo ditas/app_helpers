b0cbf924f2f45bce60e9aa9006ac565880f76bfc
ditas <ditasandditas@gmail.com>
2019-02-12 16:09:29 +0300

    types added

--- src/types_helper.erl
+++ src/types_helper.erl
@@ -13,7 +13,9 @@
 -export([
     val_to_list/1,
     val_to_float/1,
-    val_to_integer/1
+    val_to_integer/1,
+    list_to_date/1,
+    list_to_datetime/1
 ]).
 
 val_to_list(Val) when is_float(Val) ->
@@ -67,3 +69,15 @@ is_numeric(Val) ->
     Float = (catch binary_to_float(Val)),
     Int = (catch binary_to_integer(Val)),
     is_number(Float) orelse is_number(Int).
+
+list_to_date(List) ->
+    Bin = list_to_binary(List),
+    [Y, M, D] = binary:split(Bin, <<"-">>, [global]),
+    {binary_to_integer(Y), binary_to_integer(M), binary_to_integer(D)}.
+
+list_to_datetime(List) ->
+    Bin = list_to_binary(List),
+    [BinDate, BinTime] = binary:split(Bin, <<" ">>, [global]),
+    [Y, M, D] = binary:split(BinDate, <<"-">>, [global]),
+    [H, Min, S] = binary:split(BinTime, <<":">>, [global]),
+    {{binary_to_integer(Y), binary_to_integer(M), binary_to_integer(D)}, {binary_to_integer(H), binary_to_integer(Min), binary_to_integer(S)}}.
